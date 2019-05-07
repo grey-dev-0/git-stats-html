034638aa47ce9badbb911f0996448b9bd26fb238
Mohyaddin Saleh <mo7y.66@gmail.com>
2019-05-08 01:24:25 +0300

    Added months labels, and properly set styling for the output file.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var $ = require('jquery');
 
 const colors = ['#ebedf0', '#c6e48b', '#7bc96f', '#239a3b', '#196127'];
 let dom = new JSDOM('<!DOCTYPE html><html lang="en"><head><title>Git Report</title>'+
-    '<style>#grid .title{height:16px;font-size:12px}#grid #hcol{display:inline-block;vertical-align:top}#grid #hcol .title:first-child{margin-bottom:6px}#grid #hcol .title{margin-right:6px}#grid .col{display:inline-block;width:16px}#grid .col .title{margin-bottom:6px}#grid .col .day{width:16px;height:16px}</style>'+
+    '<style>#grid .title{height:16px;font-size:12px}#grid #hcol{display:inline-block;vertical-align:top}#grid #hcol .title:first-child{margin-bottom:6px}#grid #hcol .title{margin-right:6px;padding-top:2px}#grid .col{display:inline-block;width:16px;vertical-align:top}#grid .col .title{margin-bottom:6px}#grid .col .day{width:16px;height:16px;border:1px #fff solid}#container{display:inline-block;margin:auto;min-width:900px;border:1px #404040 solid;border-radius:8px;padding:12px 12px 0}#container .info{display:inline-block;width:33.333%;text-align:center;margin:12px 0 12px}</style>'+
     '</head><body></body></html>');
 $ = $(dom.window);
 let document = dom.window.document;
@@ -25,6 +25,7 @@ process.stdin.on('end', function(){
         fs.writeFileSync(argInput[0], dom.serialize());
     else
         fs.writeFileSync('git-stats.htm', dom.serialize());
+    fs.writeFileSync('git-stats.json', pipeInput);
 });
 
 function render(data){
@@ -37,13 +38,14 @@ function render(data){
     let container = $('<div id="container"><div id="grid"><div id="hcol"><div class="title"></div><div class="title">'+
         '</div><div class="title">Mon</div><div class="title"></div><div class="title">Wed</div><div class="title"></div>'+
         '<div class="title">Fri</div><div class="title"></div></div></div><div class="info"><strong>Total Commits</strong><br/>'
-        +commits.length+'</div><div class="info"><strong>Current Streak</strong><br/>'+currentStreakCount+' Days, from '
+        +commits.length+' Commits,<br/>from '+moment(data.start).format('YYYY-MM-DD')+' to '+moment(data.end).format('YYYY-MM-DD')
+        +'</div><div class="info"><strong>Current Streak</strong><br/>'+currentStreakCount+' Days,<br/>from '
         +moment(currentStreakPeriod.start).format('YYYY-MM-DD')+' to '+moment(currentStreakPeriod.end).format('YYYY-MM-DD')
-        +'</div><div class="info"><strong>Longest Streak</strong><br/>'+longestStreakCount+' Days, from '
+        +'</div><div class="info"><strong>Longest Streak</strong><br/>'+longestStreakCount+' Days,<br/>from '
         +moment(longestStreakPeriod.start).format('YYYY-MM-DD')+' to '+moment(longestStreakPeriod.end).format('YYYY-MM-DD')
         +'</div></div>');
 
-    let grid = container.find('#grid');
+    let grid = container.find('#grid'), lastMonth = null;
     commits.forEach(function(commit, i){
         let timestamp = moment(commit[0]);
         let record = {
@@ -59,6 +61,11 @@ function render(data){
                 column.append('<div class="day">').find('.day:last').css('background-color', colors[0]).attr('title','n/a');
         column.append('<div class="day">').find('.day:last').css('background-color', colors[record.level])
             .attr('title', record.date+': '+record.count);
+        let month = moment(record.date).format('MMM');
+        if(lastMonth != month){
+            lastMonth = month;
+            column.find('.title').text(month);
+        }
     });
 
     let body = $(document.getElementsByTagName('body')[0]);
